1fe94a3baaca2998480b80385c3ea4503dfa6deb
樋口　創 <31063@ojsacjp.onmicrosoft.com>
2021-05-28 15:39:15 +0900

    P79　演習問題　完成版

---
+++ Exercise3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SalesCalculator {
+    class Program {
+        static void Main(string[] args) {
+            var sales = new SalesCounter("Sales.csv");
+
+            var amountPerStore = sales.GetPerStoreSales();
+            foreach(var obj in amountPerStore) {
+                Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+            }
+        }
+
+        
+    }
+}
---
+++ Exercise3/SalesCounter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator {
+    // 売上集計クラス
+    public class SalesCounter {
+        private IEnumerable<Sale> _sales;
+
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
+        }
+
+
+        // リスト2-15
+        // 売上データを読み込み、saleオブジェクトのリストを返す
+        private static IEnumerable<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale
+                {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
+
+
+        // 店舗売り上げを求める
+        public IDictionary<string,int> GetPerStoreSales() {
+            var dict = new Dictionary<string, int>();
+            // ContainsKeyは店舗名がコレクションの中にあるかどうかを調べている。
+            foreach (var sale in _sales) {
+                if (dict.ContainsKey(sale.ProductCategory))
+                    // すでにコレクションに店舗が設定されている
+                    dict[sale.ProductCategory] += sale.Amount;
+                else
+                    // コレクションに店舗を登録
+                    dict[sale.ProductCategory] = sale.Amount;
+            }
+            return dict;
+        }
+    }
+}
