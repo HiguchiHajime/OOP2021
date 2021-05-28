f55090b1eb37e8ad6bc25ae36647c94c5267b4a0
樋口　創 <31063@ojsacjp.onmicrosoft.com>
2021-05-28 15:56:02 +0900

    P79　Execise1　改訂版

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -7,19 +7,18 @@ using System.Threading.Tasks;
 namespace Exercise1 {
     class Program {
         static void Main(string[] args) {
-            Song[] songs = new Song[3];
-            songs[0] = new Song("カリカリ", "梅", 250);
-            songs[1] = new Song("IPA", "応用", 800);
-            songs[2] = new Song("バドワイザー", "ビールサーバー", 120);
-
+            var songs = new Song[] {
+             new Song("カリカリ", "梅", 250),
+             new Song("IPA", "応用", 800),
+             new Song("バドワイザー", "ビールサーバー", 120)
+        };
             PrintSongs(songs);
         }
 
         //2.1.4
         private static void PrintSongs(Song[] songs) {
-
-            for (int i = 0; i < songs.Length; ++i) {
-                Console.WriteLine("曲名:{0}    アーティスト名:{1}    再生時間:{2}:{3:00}" ,songs[i].Title,songs[i].ArtistName,(songs[i].Length / 60),(songs[i].Length % 60));
+            foreach (var song in songs) {
+                Console.WriteLine("曲名:{0}   アーティスト名:{1}    再生時間:{2}:{3:00}" ,song.Title,song.ArtistName,(song.Length / 60),(song.Length % 60));
             }
         }
     }
