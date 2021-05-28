7105b4e879b508a0acb7e6e7fea8753700ba1805
樋口　創 <31063@ojsacjp.onmicrosoft.com>
2021-05-28 15:08:26 +0900

    問題2.1　完成

---
+++ Exercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+            Song[] songs = new Song[3];
+            songs[0] = new Song("カリカリ", "梅", 250);
+            songs[1] = new Song("IPA", "応用", 800);
+            songs[2] = new Song("バドワイザー", "ビールサーバー", 120);
+
+            PrintSongs(songs);
+        }
+
+        //2.1.4
+        private static void PrintSongs(Song[] songs) {
+
+            for (int i = 0; i < songs.Length; ++i) {
+                Console.WriteLine("{0} {1} {2}:{3:00}" ,songs[i].ArtistName,songs[i].Title,(songs[i].Length / 60),(songs[i].Length % 60));
+            }
+        }
+    }
+}
---
+++ Exercise1/Song.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+     class Song {
+        // 曲のタイトル
+        public string Title = null;
+        // アーティスト名
+        public string ArtistName = null ;
+        // 曲の長さ
+        public int Length = 0;
+
+        public Song(string title,string name,int len) {
+            Title = title;
+            ArtistName = name;
+            Length = len;
+        }
+
+    }
+}
