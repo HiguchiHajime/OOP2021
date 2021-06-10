5b6e730ee3f621b60da54601a9d8d94c1587d5f0
樋口　創 <31063@ojsacjp.onmicrosoft.com>
2021-06-10 15:58:05 +0900

    P134　演習問題　途中

---
+++ Chapter04/Exercise/Exercise2/Program.cs
@@ -0,0 +1,44 @@
+﻿using Exercise1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+            // 4.2.1
+            var ym = new YearMonth[] {
+                new YearMonth(1980,1),
+                new YearMonth(1990,7),
+                new YearMonth(2001,8),
+                new YearMonth(2020,5),
+                new YearMonth(2021,8),
+                };
+            // 4.2.2
+            Exercise2_2(ym);
+            Console.WriteLine("------");
+
+
+        }
+
+
+
+        private static void Exercise2_2(YearMonth[] ym) {
+            foreach (var yemo in ym) {
+                Console.WriteLine(yemo);
+            };
+        }
+
+        // 4.2.3
+        static YearMonth FindFirst21C(YearMonth[] ym) {
+            foreach (var YM in ym) {
+                if (YM.Is21Centurury) {
+                    return YM;
+                }
+            }
+            return null;
+        }
+    }
+}
--- Chapter04/Section01/Program.cs
+++ Chapter04/Section01/Program.cs
@@ -30,27 +30,54 @@ namespace Section01 {
             //private static object GetMessage(object code) {
             //    return null;
             //}
-            #endregion  
-
+            #endregion
+            // 前置詞後置詞など
+            #region
             //var ret = GetProductA();
 
-            int count = 0;
-            Console.WriteLine($"後置：{count++ }");
-            Console.WriteLine($"前置：{++count }");
-        }
-        private static Product GetProductA() {
+            //int count = 0;
+            //Console.WriteLine($"後置：{count++ }");
+            //Console.WriteLine($"前置：{++count }");
+
+
+            //var str = "123";
+            //int height;
+            //if (int.TryParse(str,out height)) {
+            //    Console.WriteLine(height);
+            //}
+            //else {
+            //    Console.WriteLine("変換できません");
+            //}
+            //
+            #endregion
+
+            var Session = new Dictionary<string, object>();
+            Session["MyProduct"] = new Product();
+
+            var product = Session["MyProduct"] as Product;
+            if(product == null) {
+                // productが取得できなかった時の処理
+                Console.WriteLine("productが取得できなかった");
+            }
+            else {
+                // productが取得できた時の処理
+                Console.WriteLine("productが取得できた");
+            }
+
+}
+            private static Product GetProductA() {
             Sale sale = new Sale();
             sale.product = new Product();
             //sale = null;
             return sale?.product;  // null条件演算子
-        }
-
-    }
-        class Sale {
+            }
+          class Sale {
             public string ShopName { get; set; }
             public int Amount { get; set; }
             public Product product { get; set; }
-        }
+          }
 
+    }
+ 
 }
 
