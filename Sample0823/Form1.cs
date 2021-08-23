2de4cec7c0ae9d3abd7f46e4eecd497264e7a4e3
創 樋口 <31063@ojsacjp.onmicrosoft.com>
2021-08-23 15:52:25 +0900

    リモート授業で作ったサンプル0823

---
+++ Sample0823/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample0823 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Exec_Click(object sender, EventArgs e) {
+            //int num = int.Parse(Value.Text);
+            //for(int i = 2; i<= int.Parse(Jyou.Text); i++) {
+            //    num *= int.Parse(Value.Text);
+            //}
+            //Result.Text = num.ToString();
+
+            Result.Text = Math.Pow(int.Parse(Value.Text), int.Parse(Jyou.Text)).ToString();
+
+        }
+    }
+}
