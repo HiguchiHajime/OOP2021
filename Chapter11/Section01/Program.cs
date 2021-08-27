c595a7aadb98017d92ac047b874128942ba85fc2
創 樋口 <31063@ojsacjp.onmicrosoft.com>
2021-08-27 13:41:36 +0900

    リスト11.9

--- Chapter11/Section01/Program.cs
+++ Chapter11/Section01/Program.cs
@@ -9,16 +9,21 @@ namespace Section01 {
     class Program {
         static void Main(string[] args) {
             var xdoc = XDocument.Load("novelists.xml");
-            //var xelements = xdoc.Root.Elements().Where(x => ((DateTime)x.Element("birth")).Year >= 1900);
-            //var xelements = xdoc.Root.Elements().OrderBy(x => (string)(x.Element("name").Attribute("kana")));
-            foreach (var xnovelist in xdoc.Root.Elements()) {
-                var xname = xnovelist.Element("name");
-                var works = xnovelist.Element("masterpieces")
-                                     .Elements("title")
-                                     .Select(x => x.Value);
-                Console.WriteLine("{0} {1}",xname.Value,string.Join(",",works));
+            var novelists = xdoc.Root.Elements()
+                                .Select(x => new
+                                {
+                                    Name = (string)x.Element("name"),
+                                    Birth = (DateTime)x.Element("birth"),
+                                    Death = (DateTime)x.Element("death")
+                                });
+            foreach (var novelist in novelists) {
+                //var xname = xnovelist.Element("name");
+                //var works = xnovelist.Element("masterpieces")
+                //                     .Elements("title")
+                //                     .Select(x => x.Value);
+                Console.WriteLine("{0} ({1}-{2})",novelist.Name,novelist.Birth.Year,novelist.Death.Year);
 
+                }
             }
-        }
     }
 }
