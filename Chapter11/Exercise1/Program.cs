7926166d06954275c1f8d0c1574814c567a67c68
創 樋口 <31063@ojsacjp.onmicrosoft.com>
2021-08-27 15:17:13 +0900

    Exercise1_1

---
+++ Chapter11/Exercise1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+            var file = "Sample.xml";
+            Exercise1_1(file);
+            Console.WriteLine("-----------");
+            Exercise1_2(file);
+            Console.WriteLine("-----------");
+            Exercise1_3(file);
+            Console.WriteLine("-----------");
+
+        }
+
+        private static void Exercise1_1(string file) {
+            var xdoc = XDocument.Load(file);
+            var xelements = xdoc.Root.Elements();
+
+            foreach(var xsports in xelements) {
+                var xname = xsports.Element("name");
+                var xmember = xsports.Element("teammembers");
+
+                Console.WriteLine("競技名:{0},メンバー数:{1}",xname.Value,xmember.Value);
+            }
+ 
+        }
+
+        private static void Exercise1_2(string file) {
+            var xdoc = XDocument.Load(file);
+            var xsports = xdoc.Root.Elements().OrderBy(x => x.Element("firstplayed"));
+            foreach(var xsport in xsports) {
+                var xname = xsport.Element("name").Attribute("kanji");
+                var xyear = xsport.Element("firstplayed");
+                Console.WriteLine("競技名:{0},最初にプレーされた年:{1}", xname.Value, xyear.Value);
+            }
+
+        }
+
+        private static void Exercise1_3(string file) {
+        }
+    }
+}
