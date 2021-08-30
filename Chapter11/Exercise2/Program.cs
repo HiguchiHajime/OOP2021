2a8323eec2375b470a2ae954d4ab632295b8c41f
創 樋口 <31063@ojsacjp.onmicrosoft.com>
2021-08-30 15:59:46 +0900

    問題11.2　途中

---
+++ Chapter11/Exercise2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+            var xdoc = XDocument.Load("11_2.xml");
+            var newfile = "kanji.xml";
+            var pairs = xdoc.Root.Elements()
+                                 .Select(x => new
+                                 {
+                                     Key = x.Name.LocalName,
+                                     Value = x.Value
+                                 });
+
+            var dict = pairs.ToDictionary(x => x.Key, x => x.Value);
+
+            var query = dict.Select(s => new XElement("word",
+                                         new XAttribute("kanji", s.Key),
+                                         new XAttribute("yomi", s.Value)));
+            var root = new XElement("difficultkanji", query);
+
+            xdoc.Root.Add(root);
+            xdoc.Save("kanji.xml");
+             
+        }
+    }
+}
