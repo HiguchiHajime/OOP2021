c5b605d27118d8bc981a304419ca8199dae34795
創 樋口 <31063@ojsacjp.onmicrosoft.com>
2021-11-18 09:25:04 +0900

    数あてゲーム

--- WPF/NumberGame/MainWindow.xaml.cs
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -22,12 +22,43 @@ namespace NumberGame {
     public partial class MainWindow : Window {
         private int Anser;
         Stopwatch sw = new Stopwatch();
+        private int Row = 5;
+        private int Column = 5;
 
         public MainWindow() {
             InitializeComponent();
-           
+            
         }
 
+        private void Create_Button(int row , int column) {
+            for (int j = 0; j < column; j++) {
+                this.NumGame.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int i = 0; i < row +1; i++) {
+                this.NumGame.RowDefinitions.Add(new RowDefinition());
+               
+            }
+
+            
+
+            var count = 1;
+
+            for (int k = 0; k < column; k++) {
+                for (int i = 0; i < row; i++) {
+                    Button NButton = new Button();
+                    NButton.SetValue(Grid.ColumnProperty, i);
+                    NButton.SetValue(Grid.RowProperty, k+1);
+                    NButton.Content = count;
+                    NButton.Click += Button_Click;
+                    this.NumGame.Children.Add(NButton);
+                    count++;
+                }
+            }
+
+            this.NumberText.SetValue(Grid.RowProperty,Row);
+
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
             sw.Start();
@@ -61,7 +92,8 @@ namespace NumberGame {
 
         private void Window_Loaded(object sender, RoutedEventArgs e) {
             Random num =  new Random();
-            Anser = num.Next(1,25);
+            Create_Button(Row, Column);
+            Anser = num.Next(1,Row*Column);
         }
     }
 }
